96238837cad3e37e55fa2b6b6550bce52a9a429e
gasparnd <gaspardolcemascolo@gmail.com>
2025-01-24 20:17:18 -0300

    Initial Commit

---
+++ BanquitoScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BanquitoScan
+//
+//  Created by Gaspar Dolcemascolo on 24/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
